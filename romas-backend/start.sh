ad70c75724034d3a28f987ed03e1daf2e8a17ef5
Flodias <adodemeflodias@gmail.com>
2026-07-31 22:15:19 +0100

    hdhdh

--- romas-backend/start.sh
+++ romas-backend/start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "Lancement des migrations..."
+# Nettoyage OBLIGATOIRE du cache pour prendre en compte
+# les modifications de config/cors.php et routes/api.php
+php artisan config:clear
+php artisan route:clear
+php artisan cache:clear
+
+# Migrations et données de test
 php artisan migrate --force
+php artisan db:seed --force
 
-echo "Mise en cache de la configuration..."
-php artisan config:cache
-php artisan route:cache
-
-echo "Démarrage d'Apache..."
-sed -i "s/80/${PORT:-80}/g" /etc/apache2/ports.conf /etc/apache2/sites-available/000-default.conf
+# Lancer Apache
 apache2-foreground
